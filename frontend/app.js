9a1112665d0a0df5794488700a79f667d55b59b3
Jesse Li <jessejesse123@gmail.com>
2018-03-21 13:03:17 -0400

    open sourcing this jawn

---
+++ frontend/app.js
@@ -0,0 +1,67 @@
+/* Initial setup */
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x427af4);
+var camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1800 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.domElement.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+});
+
+
+/* Point field */
+var pointsGeometry = new THREE.Geometry();
+window.points.forEach(function(coords) {
+  var point = new THREE.Vector3();
+  point.x = coords[0];
+  point.y = coords[1];
+  point.z = coords[2];
+  pointsGeometry.vertices.push(point);
+});
+
+var material = new THREE.PointsMaterial({
+  color: 0xff7072,
+  size: 3,
+});
+
+var pointField = new THREE.Points(pointsGeometry, material);
+scene.add(pointField);
+
+
+/* Bottom plane */
+var planeGeometry = new THREE.PlaneGeometry(150, 150);
+var planeMaterial = new THREE.MeshBasicMaterial({
+  side: THREE.DoubleSide,
+  transparent: true,
+  opacity: 0.2,
+});
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.set(-Math.PI/2, Math.PI/2000, Math.PI);
+plane.position.x = 50;
+plane.position.z = 70;
+plane.position.y = -20;
+scene.add(plane);
+
+
+/* Controls */
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.minDistance = 700;
+controls.maxDistance = 1400;
+controls.maxAzimuthAngle = 1.7;
+controls.minAzimuthAngle = -0.2;
+controls.maxPolarAngle = 1.7;
+
+var center = new THREE.Vector3(center[0], center[1], center[2]);
+controls.target = center;
+
+controls.update();
+
+
+/* Render loop */
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
---
+++ img2dots.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+from PIL import Image
+import numpy as np
+
+def discretize(pixels):
+    """Transforms a 3D array of pixel values into a 2D array
+
+    Args:
+        pixels: 3D array containing [R, G, B] values of each pixel
+
+    Returns:
+        a 2D array of boolean values. Colored pixels = True, white = False
+    """
+    return np.invert(pixels.transpose().sum(axis=0).transpose().astype(bool))
+
+front = discretize(np.array(Image.open("front.png")))
+side = discretize(np.array(Image.open("side.png")))
+
+assert(front.shape[0] == side.shape[0]) # Images must be the same height
+
+# Build point coordinates from images
+# Ugly nesting but it works
+points = []
+for y in range(front.shape[0]):
+    for x in range(front.shape[1]):
+        if front[y][x]:
+            for z in range(side.shape[1]):
+                if side[y][z]:
+                    points.append([x, front.shape[0] - y, side.shape[1] - z])
+
+center = [front.shape[1] / 2, front.shape[0] / 2, side.shape[1] / 2]
+
+# add random noise to disrupt grid pattern
+points = [list(point + np.random.normal(0, 0.2, 3)) for point in points]
+
+print(f"window.points = {points}; window.center = {center}")
